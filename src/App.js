fb75b79fe4fec04da0ff9d7f5c296ace5b0d84c3
Duong Van Thien <duongvanthienbkhoa@gmail.com>
2020-08-31 14:25:18 +0700

    add antd

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Button } from 'antd';
 import './App.css';
 
 import BookList from './components/BookList';
@@ -7,6 +8,7 @@ import BookList from './components/BookList';
 function App() {
   return (
     <div className="App">
+      <Button type="primary">Thêm</Button>
       <BookList />
     </div>
   );
